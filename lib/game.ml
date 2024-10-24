1f1423d785efbb7e8c7f9e5ad7b47c07af10b545
Eva Herson <evaherson@MacBook-Pro-de-Eva.local>
2024-10-24 11:41:29 +0200

    - player pay owner when they are on a square with owner

--- lib/game.ml
+++ lib/game.ml
@@ -7,6 +7,7 @@ type play =
   | Roll
   | Move of int
   | Buy of square_buyable
+  | PayOwner of square_buyable
 
 type game_state = 
   { board : board;
@@ -15,6 +16,7 @@ type game_state =
   }
 
   type outcome = 
+  | HandleSquare of square
   | Next of game_state
   | Error of error
 (*| | Endgame of player option*)
@@ -22,9 +24,13 @@ type game_state =
 let roll_dices () = 
   let (d1, d2) = (Random.int 6 + 1,
   Random.int 6 + 1) in 
+  print_endline "";
   print_endline ("Résultat des dés : " ^ string_of_int d1 ^ " , " ^ string_of_int d2);
   (d1, d2)
-  
+
+
+
+
 (* Handle the int option when finding the index of player *)
 let handle_index_player player game_state f  = 
   match find_index_player player game_state.players with
@@ -33,6 +39,8 @@ let handle_index_player player game_state f  =
 
 let end_turn game_state = 
     Next {game_state with current_index_player = (game_state.current_index_player + 1) mod Array.length game_state.players}
+
+
 
 let rec act player play game_state = 
   match play with
@@ -46,12 +54,9 @@ let rec act player play game_state =
       handle_index_player player game_state (fun index ->
 
         game_state.players.(index) <- player; 
-        (let handle_square_result player game_state (square : square) =
-          match square with
-          | Buyable square_buyable -> act player (Buy square_buyable) game_state
-          | _ -> end_turn game_state
-           in handle_square_result player game_state game_state.board.(pos_player player))
-        )
+        display game_state.board game_state.players;
+
+        HandleSquare (game_state.board.(pos_player player)))
 
   (* achat d'une propriété *)
    | Buy square_buyable -> 
@@ -64,8 +69,21 @@ let rec act player play game_state =
             game_state.board.(pos_player player) <- square;
             (* TODO : pas dans la même fonction*)
             print_endline (name_player player ^ " a acheté " ^ string_of_int (price_buyable square_buyable.type_square) ^ "€ " ^ name_square square);
-            end_turn game_state
+            end_turn game_state  
       
+  (* paiement au propriétaire *)
+  | PayOwner square_buyable -> 
+    (match get_owner square_buyable with
+    | Some owner -> 
+      if money_player player < price_buyable square_buyable.type_square then Error (NotEnoughMoney)
+      else 
+        change_money player (- (price_buyable square_buyable.type_square)) |> fun player -> 
+        change_money owner (price_buyable square_buyable.type_square) |> fun owner -> 
+        game_state.players.(game_state.current_index_player) <- player;
+        game_state.players.(find_index_player owner game_state.players |> Option.get) <- owner;
+        print_endline (name_player player ^ " a payé " ^ name_player owner ^ " " ^ string_of_int (price_buyable square_buyable.type_square) ^ "€");
+        end_turn game_state
+    | None -> Error (NoOwner))
 
   (* | End -> end_turn player game_state *)
   (* | Pay -> pay player game_state *)
@@ -78,3 +96,12 @@ let create_game board players =
   ; players
   ; current_index_player = 0
   }
+
+(* demande d'achat d'une propriété *)
+let ask_buy square_buyable =
+  (print_endline ("Voulez-vous acheter " ^ name_square (Buyable (square_buyable)) ^ " pour " ^ string_of_int (price_buyable square_buyable.type_square) ^ "€ ? (y/n)");
+  let rec ask_buy () = 
+    match read_line () with
+    | "y" -> true
+    | "n" -> false
+    | _ -> print_endline "Veuillez entrer y ou n"; ask_buy () in ask_buy ())
--- lib/game.mli
+++ lib/game.mli
@@ -8,11 +8,15 @@ type play =
   | Roll
   | Move of int
   | Buy of Square.square_buyable
+  | PayOwner of Square.square_buyable
   
  
 type outcome = 
+  | HandleSquare of Square.square
   | Next of game_state
   | Error of Error.error
 
 val act : Player.player -> play -> game_state -> outcome
 val create_game : Board.board -> Player.player array -> game_state
+val end_turn : game_state -> outcome
+val ask_buy : Square.square_buyable -> bool
