7044c9aba9f634dc9c6fc82b2ada431e9fb67525
Grimmins <lukas.koltes@hotmail.fr>
2024-11-03 15:21:50 +0100

    - hot fix dices

--- lib/game.ml
+++ lib/game.ml
@@ -56,7 +56,7 @@ let roll_dices () =
   print_endline "";
   print_endline ("Résultat des dés : " ^ string_of_int d1 ^ " , " ^ string_of_int d2);
   if d1 = d2 then print_endline "Double !";
-  (1, 0)
+  (d1, d2)
 
 (* Handle the int option when finding the index of player *)
 let handle_index_player player game_state f  = 
