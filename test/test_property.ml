deb196e58e4bf9e284e51cbe8ce3ce053b0556e1
Grimmins <lukas.koltes@hotmail.fr>
2024-11-05 12:44:22 +0100

    - tests  -> square 69 % coverage  -> property 100 % coverage

--- test/test_property.ml
+++ test/test_property.ml
@@ -3,73 +3,286 @@ open Unipoly
 open Square
 open Property
 
-(* Fonction auxiliaire pour définir un propriétaire sur un Buyable square *)
-let set_owner square owner_index =
-  match square with
-  | Buyable b -> Buyable { b with proprietaire_index = Some owner_index }
-  | _ -> square
+(* Fonctions auxiliaires pour les tests *)
+let set_owner square_buyable owner_index =
+  match square_buyable with
+  | { proprietaire_index = _; type_square } ->
+    { proprietaire_index = Some owner_index; type_square }
 
-(* Créons des exemples de propriétés en utilisant les fonctions de Square.ml pour retourner directement des valeurs de type square *)
-let restaurant1 = set_owner (create_restaurant "Restaurant1") 0
-let restaurant2 = set_owner (create_restaurant "Restaurant2") 1
-let library1 = set_owner (create_library "Library1") 0
-let library2 = set_owner (create_library "Library2") 1
-let cours1 = set_owner (create_cours Math 100 "Maths 101" 10 30 60 100 20) 0
-let cours2 = set_owner (create_cours Info 150 "Info 101" 15 40 75 120 25) 1
-let house = House  (* Une propriété non achetable *)
+(* Création des types `square_buyable` en utilisant les fonctions de Square.ml *)
+let example_library = set_owner (Option.get (get_square_buyable (create_library "Bibliothèque"))) 0
+let example_restaurant = set_owner (Option.get (get_square_buyable (create_restaurant "Restaurant"))) 0
+let example_course_1 = set_owner (Option.get (get_square_buyable (create_cours Math 100 "Mathématiques 1" 20 40 80 160 50))) 0
+let example_course_2 = set_owner (Option.get (get_square_buyable (create_cours Math 100 "Mathématiques 2" 20 40 80 160 50))) 0
+let example_course_3 = set_owner (Option.get (get_square_buyable (create_cours Physique 100 "Physique 2" 20 40 80 160 50))) 0
+let example_course_4 = set_owner (Option.get (get_square_buyable (create_cours Physique 100 "Physique 1" 8 40 80 160 50))) 0
+let example_course_5 = Option.get (get_square_buyable (create_cours Physique 100 "Physique 3" 20 40 80 160 50))
+let example_course_info_1 = set_owner (Option.get (get_square_buyable (create_cours Info 100 "Info 1" 10 40 80 160 50))) 0
+let example_course_info_2 = set_owner (Option.get (get_square_buyable (create_cours Info 100 "Info 2" 12 40 80 160 50))) 0
 
-(* Créer le tableau example_board avec des éléments de type square *)
-let example_board = [|
-  restaurant1;
-  library1;
-  cours1;
-  restaurant2;
-  library2;
-  cours2;
-  house
+
+(* Utiliser update_degre pour simuler des cours avec différents diplômes *)
+let set_course_to_degree course_buyable degree =
+  match degree with
+  | None -> course_buyable
+  | Some Licence -> update_degre course_buyable
+  | Some Master -> update_degre (update_degre course_buyable)
+  | Some Doctorat -> update_degre (update_degre (update_degre course_buyable))
+
+let course_with_licence = set_course_to_degree example_course_1 (Some Licence)
+let course_with_master = set_course_to_degree example_course_2 (Some Master)
+let course_with_doctorat = set_course_to_degree example_course_3 (Some Doctorat)
+
+(* Créer le plateau de test avec des `square` en utilisant `Buyable` pour encapsuler chaque `square_buyable` *)
+let board_example = [|
+  Buyable example_library;
+  Buyable example_restaurant;
+  Buyable course_with_licence;
+  Buyable course_with_master;
+  Buyable course_with_doctorat;
+  Buyable example_course_4;
+  Buyable example_course_5;
+  Buyable example_course_info_1;
+  Buyable example_course_info_2;
+  House  (* Case non achetable *)
 |]
 
-(* Test pour is_restaurant *)
+(* Tests *)
+
+(* Test de la fonction is_restaurant *)
 let test_is_restaurant () =
-  check bool "Restaurant1 est un restaurant" true (is_restaurant (get_type_square (Option.get (get_square_buyable restaurant1))));
-  check bool "Library1 n'est pas un restaurant" false (is_restaurant (get_type_square (Option.get (get_square_buyable library1))))
+  check bool "Est un restaurant" true (is_restaurant (get_type_square (Option.get (get_square_buyable (board_example.(1))))));
+  check bool "N'est pas un restaurant" false (is_restaurant (get_type_square (Option.get (get_square_buyable (board_example.(0))))))
 
-(* Test pour is_library *)
+(* Test de la fonction is_library *)
 let test_is_library () =
-  check bool "Library1 est une bibliothèque" true (is_library (get_type_square (Option.get (get_square_buyable library1))));
-  check bool "Cours1 n'est pas une bibliothèque" false (is_library (get_type_square (Option.get (get_square_buyable cours1))))
+  check bool "Est une bibliothèque" true (is_library (get_type_square (Option.get (get_square_buyable (board_example.(0))))));
+  check bool "N'est pas une bibliothèque" false (is_library (get_type_square (Option.get (get_square_buyable (board_example.(1))))))
 
-(* Test pour is_cours *)
+(* Test de la fonction is_cours *)
 let test_is_cours () =
-  check bool "Cours1 est un cours" true (is_cours (get_type_square (Option.get (get_square_buyable cours1))));
-  check bool "Restaurant1 n'est pas un cours" false (is_cours (get_type_square (Option.get (get_square_buyable restaurant1))))
+  check bool "Est un cours" true (is_cours (get_type_square (Option.get (get_square_buyable (board_example.(2))))));
+  check bool "N'est pas un cours" false (is_cours (get_type_square (Option.get (get_square_buyable (board_example.(1))))))
 
-(* Test pour count_restaurants_owned *)
-let test_count_restaurants_owned () =
-  let count = count_restaurants_owned 0 example_board in
-  check int "Nombre de restaurants détenus par le joueur 0" 1 count
+(* Test pour count_properties_owned *)
+let test_count_properties_owned () =
+  let count_libs = count_librairies_owned 0 board_example in
+  let count_rests = count_restaurants_owned 0 board_example in
+  let count_courses = count_cours_owned 0 board_example in
+  check int "Nombre de bibliothèques possédées par joueur 0" 1 count_libs;
+  check int "Nombre de restaurants possédés par joueur 0" 1 count_rests;
+  check int "Nombre de cours possédés par joueur 0" 6 count_courses
 
-(* Test pour count_librairies_owned *)
-let test_count_librairies_owned () =
-  let count = count_librairies_owned 0 example_board in
-  check int "Nombre de bibliothèques détenues par le joueur 0" 1 count
+(* Test pour owns_all_courses_in_ufr *)
+let test_owns_all_courses_in_ufr () =
+  check bool "Possède tous les cours en Math" true (owns_all_courses_in_ufr Math 0 board_example);
+  check bool "Ne possède pas tous les cours en Physique" false (owns_all_courses_in_ufr Physique 0 board_example)
 
-(* Test pour count_cours_owned *)
-let test_count_cours_owned () =
-  let count = count_cours_owned 0 example_board in
-  check int "Nombre de cours détenus par le joueur 0" 1 count
+(* Test pour get_courses_owned_by_player *)
+let test_get_courses_owned_by_player () =
+  let courses = get_courses_owned_by_player 0 board_example in
+  check int "Nombre de cours possédés par le joueur 0" 6 (List.length courses)
 
-(* Groupe de tests pour Property *)
+(* Test pour get_properties_owned_by_player *)
+let test_get_properties_owned_by_player () =
+  let properties = get_properties_owned_by_player 0 board_example in
+  check int "Nombre de propriétés possédées par le joueur 0" 8 (List.length properties)
+
+(* Test pour get_adjusted_course_landing_price avec tous les cours dans un UFR sans diplôme *)
+let test_get_adjusted_course_landing_price_all_courses_owned () =
+  (* On s'assure que tous les cours dans Math sont possédés par le joueur 0 sans diplôme *)
+  let course_phys_1 = get_cours_from_square (board_example.(5)) in
+  let course_info_1 = get_cours_from_square (board_example.(7)) in
+  let course_info_2 = get_cours_from_square (board_example.(8)) in
+  let price_info_1 = get_adjusted_course_landing_price course_info_1 0 board_example in
+  let price_info_2 = get_adjusted_course_landing_price course_info_2 0 board_example in
+  let price_phys_1 = get_adjusted_course_landing_price course_phys_1 0 board_example in
+  (* Vérification que le prix est doublé car le joueur possède tous les cours dans Math sans diplôme *)
+  check int "Prix ajusté pour cours Physique 1 sans diplôme, pas tous les cours de l'UFR possédés" 8 price_phys_1;
+  check int "Prix ajusté pour cours Info 1 sans diplôme, tous les cours de l'UFR possédés" 20 price_info_1;
+  check int "Prix ajusté pour cours Info 2 sans diplôme, tous les cours de l'UFR possédés" 24 price_info_2
+
+
+(* Test pour get_landing_price selon le diplôme *)
+let test_get_landing_price_with_degrees () =
+  (* Initialisation de cours avec différents diplômes *)
+  let cours_without_diploma = get_cours_from_square (Buyable example_course_4) in
+  let cours_licence = get_cours_from_square (Buyable course_with_licence) in
+  let cours_master = get_cours_from_square (Buyable course_with_master) in
+  let cours_doctorat = get_cours_from_square (Buyable course_with_doctorat) in
+  check int "Prix landing pour cours sans diplôme" 8 (get_landing_price cours_without_diploma);
+  check int "Prix landing pour cours avec Licence" 40 (get_landing_price cours_licence);
+  check int "Prix landing pour cours avec Master" 80 (get_landing_price cours_master);
+  check int "Prix landing pour cours avec Doctorat" 160 (get_landing_price cours_doctorat)
+
+(* Test pour get_next_degree_price *)
+let test_get_next_degree_price () =
+  (* Cours initial sans diplôme *)
+  let course_no_degree = get_cours_from_square (board_example.(2)) in
+  let price_no_degree = get_next_degree_price course_no_degree in
+  check int "Prix de mise à niveau pour un cours sans diplôme" 50 price_no_degree;
+
+  (* Cours avec licence *)
+  let course_with_licence = get_cours_from_square (Buyable (set_course_to_degree example_course_1 (Some Licence))) in
+  let price_with_licence = get_next_degree_price course_with_licence in
+  check int "Prix de mise à niveau pour un cours avec Licence" 50 price_with_licence;
+
+  (* Cours avec master *)
+  let course_with_master = get_cours_from_square (Buyable (set_course_to_degree example_course_1 (Some Master))) in
+  let price_with_master = get_next_degree_price course_with_master in
+  check int "Prix de mise à niveau pour un cours avec Master" 50 price_with_master;
+
+  (* Cours avec doctorat *)
+  let course_with_doctorat = get_cours_from_square (Buyable (set_course_to_degree example_course_1 (Some Doctorat))) in
+  let price_with_doctorat = get_next_degree_price course_with_doctorat in
+  check int "Prix de mise à niveau pour un cours avec Doctorat" 0 price_with_doctorat
+
+(* Tests pour get_price *)
+
+let test_get_price () =
+  (* Cas Tax *)
+  let tax_square = create_tax 150 "Taxe foncière" in
+  let tax_price = get_price board_example 0 tax_square in
+  check int "Prix pour une case de taxe" 150 tax_price;
+
+  (* Cas Restaurant (prix toujours 0) *)
+  let restaurant_price = get_price board_example 0 (Buyable example_restaurant) in
+  check int "Prix pour un restaurant" 0 restaurant_price;
+
+  (* Cas Library sans propriétaire *)
+  let library_no_owner = Option.get (get_square_buyable (create_library "Library")) in
+  let library_no_owner_price = get_price board_example 0 (Buyable library_no_owner) in
+  check int "Prix pour une bibliothèque sans propriétaire" 200 library_no_owner_price;
+
+  (* Cas Library avec propriétaire, une bibliothèque possédée *)
+  let library_one_owned_price = get_price board_example 0 (Buyable example_library) in
+  check int "Prix pour une bibliothèque avec un propriétaire possédant une bibliothèque" 25 library_one_owned_price;
+
+  (* Cas Library avec propriétaire, deux bibliothèques possédées *)
+  let library_two_owned = set_owner (Option.get (get_square_buyable (create_library "Library2"))) 0 in
+  let board_with_two_libraries = [|
+    Buyable example_library;
+    Buyable example_restaurant;
+    Buyable example_course_1;
+    Buyable example_course_2;
+    Buyable example_course_3;
+    Buyable example_course_4;
+    Buyable example_course_5;
+    Buyable library_two_owned
+  |] in
+  let library_two_owned_price = get_price board_with_two_libraries 0 (Buyable example_library) in
+  check int "Prix pour une bibliothèque avec un propriétaire possédant deux bibliothèques" 50 library_two_owned_price;
+
+  (* Cas Library avec propriétaire, trois bibliothèques possédées *)
+  let library_three_owned = set_owner (Option.get (get_square_buyable (create_library "Library3"))) 0 in
+  let board_with_three_libraries = [|
+    Buyable example_library;
+    Buyable example_restaurant;
+    Buyable example_course_1;
+    Buyable example_course_2;
+    Buyable example_course_3;
+    Buyable example_course_4;
+    Buyable example_course_5;
+    Buyable library_two_owned;
+    Buyable library_three_owned
+  |] in
+  let library_three_owned_price = get_price board_with_three_libraries 0 (Buyable example_library) in
+  check int "Prix pour une bibliothèque avec un propriétaire possédant trois bibliothèques" 100 library_three_owned_price;
+
+  (* Cas Library avec propriétaire, quatre bibliothèques possédées *)
+  let library_four_owned = set_owner (Option.get (get_square_buyable (create_library "Library4"))) 0 in
+  let board_with_four_libraries = [|
+    Buyable example_library;
+    Buyable example_restaurant;
+    Buyable example_course_1;
+    Buyable example_course_2;
+    Buyable example_course_3;
+    Buyable example_course_4;
+    Buyable example_course_5;
+    Buyable library_two_owned;
+    Buyable library_three_owned;
+    Buyable library_four_owned
+  |] in
+  let library_four_owned_price = get_price board_with_four_libraries 0 (Buyable example_library) in
+  check int "Prix pour une bibliothèque avec un propriétaire possédant quatre bibliothèques" 200 library_four_owned_price;
+
+  (* Cas IMPOSSIBLE library avec propriétaire, 5 bibliothèques possédées par exemple *)
+    let library_five_owned = set_owner (Option.get (get_square_buyable (create_library "Library5"))) 0 in
+    let board_with_five_libraries = [|
+      Buyable example_library;
+      Buyable example_restaurant;
+      Buyable example_course_1;
+      Buyable example_course_2;
+      Buyable example_course_3;
+      Buyable example_course_4;
+      Buyable example_course_5;
+      Buyable library_two_owned;
+      Buyable library_three_owned;
+      Buyable library_four_owned;
+      Buyable library_five_owned
+    |] in
+    let library_five_owned_price = get_price board_with_five_libraries 0 (Buyable example_library) in
+    check int "Prix pour une bibliothèque avec un propriétaire possédant 5 bibliothèques ce qui est impossible" 0 library_five_owned_price;
+
+  (* Cas Cours sans propriétaire *)
+  let course_no_owner = example_course_5 in
+  let course_no_owner_price = get_price board_example 0 (Buyable course_no_owner) in
+  check int "Prix pour un cours sans propriétaire" 100 course_no_owner_price;
+
+  (* Cas Cours avec propriétaire unique sans diplôme (prix doublé si tous les cours de l'UFR sont possédés) *)
+  let course_price_all_owned = get_price board_example 0 (Buyable example_course_1) in
+  check int "Prix pour un cours avec propriétaire unique sans diplôme (tous les cours de l'UFR possédés)" 40 course_price_all_owned;
+
+  (* Cas Cours avec propriétaire ayant un diplôme (licence) *)
+  let course_with_licence_price = get_price board_example 0 (Buyable course_with_licence) in
+  check int "Prix pour un cours avec une licence" 40 course_with_licence_price;
+
+  (* Cas Cours avec propriétaire ayant un master *)
+  let course_with_master_price = get_price board_example 0 (Buyable course_with_master) in
+  check int "Prix pour un cours avec un master" 80 course_with_master_price;
+
+  (* Cas Cours avec propriétaire ayant un doctorat *)
+  let course_with_doctorat_price = get_price board_example 0 (Buyable course_with_doctorat) in
+  check int "Prix pour un cours avec un doctorat" 160 course_with_doctorat_price
+
+(* Cas pour les types de square qui ne sont ni Tax ni Buyable *)
+let test_non_buyable_or_tax_price () =
+  let house_price = get_price board_example 0 House in
+  let email_price = get_price board_example 0 Email in
+  let stlife_price = get_price board_example 0 StLife in
+  check int "Prix pour une case House (non Tax ni Buyable)" 0 house_price;
+  check int "Prix pour une case Email (non Tax ni Buyable)" 0 email_price;
+  check int "Prix pour une case StLife (non Tax ni Buyable)" 0 stlife_price
+
+
+(* Test pour remove_all_properties *)
+let test_remove_all_properties () =
+  remove_all_properties 0 board_example;
+  let owned_properties = get_properties_owned_by_player 0 board_example in
+  check int "Toutes les propriétés ont été supprimées du joueur 0" 0 (List.length owned_properties)
+
+(* Groupe de tests *)
 let () =
-  run "Property tests" [
+  run "Property Tests" [
     "Type Checks", [
-      test_case "Check if property is a restaurant" `Quick test_is_restaurant;
-      test_case "Check if property is a library" `Quick test_is_library;
-      test_case "Check if property is un cours" `Quick test_is_cours;
+      test_case "Vérifie si est un restaurant" `Quick test_is_restaurant;
+      test_case "Vérifie si est une bibliothèque" `Quick test_is_library;
+      test_case "Vérifie si est un cours" `Quick test_is_cours;
     ];
-    "Count Properties", [
-      test_case "Count restaurants owned" `Quick test_count_restaurants_owned;
-      test_case "Count libraries owned" `Quick test_count_librairies_owned;
-      test_case "Count courses owned" `Quick test_count_cours_owned;
+    "Ownership Count", [
+      test_case "Compte les propriétés par type" `Quick test_count_properties_owned;
+      test_case "Vérifie si le joueur possède tous les cours en UFR" `Quick test_owns_all_courses_in_ufr;
+      test_case "Récupère les cours possédés par le joueur" `Quick test_get_courses_owned_by_player;
+      test_case "Récupère toutes les propriétés du joueur" `Quick test_get_properties_owned_by_player;
+    ];
+    "Pricing", [
+      test_case "Prix ajusté pour un cours possédé" `Quick test_get_adjusted_course_landing_price_all_courses_owned;
+      test_case "Prix de landing en fonction du diplôme" `Quick test_get_landing_price_with_degrees;
+      test_case "Prix d'upgrade du diplôme" `Quick test_get_next_degree_price;
+      test_case "Récupération du prix pour Tax / Buyable" `Quick test_get_price;
+      test_case "Récupération du prix pour =/= de Tax / Buyable" `Quick test_non_buyable_or_tax_price;
+    ];
+    "Remove Properties", [
+      test_case "Suppression de toutes les propriétés d'un joueur" `Quick test_remove_all_properties;
     ];
   ]
--- test/test_square.ml
+++ test/test_square.ml
@@ -1,79 +1,106 @@
-(*open Alcotest*)
-(*open Unipoly*)
-(*open Square*)
+open Alcotest
+open Unipoly
+open Square
 
-(*(* Test de création et accès aux attributs d'un cours *)*)
-(*(*let test_create_cours () =*)*)
-(*(*  let cours = create_cours Math 100 "Calculus" in*)*)
-(*(*  match cours with*)*)
-(*(*  | Buyable { type_square = Cours c; _ } ->*)*)
-(*(*      check string "Nom du cours" "Calculus" (get_name c);*)*)
-(*(*      check int "Prix du cours" 100 (price_buyable (Cours c));*)*)
-(*(*      check (option int) "Degré initial du cours" None (Some (match get_degre c with None -> None | Some _ -> None));*)*)
-(*(*      check (option int) "Propriétaire initial du cours" None (match cours with Buyable b -> b.proprietaire_index | _ -> None)*)*)
-(*(*  | _ -> fail "Création de cours échouée"*)*)
+(* Création de `square_buyable` en utilisant les fonctions de Square.ml *)
+let example_library = Option.get (get_square_buyable (create_library "Library"))
+let example_restaurant = Option.get (get_square_buyable (create_restaurant "Restaurant"))
+let example_course = Option.get (get_square_buyable (create_cours Math 100 "Mathématiques" 20 40 80 160 50))
 
-(*(* Test de création et accès aux attributs d'une bibliothèque *)*)
-(*(*let test_create_library () =*)*)
-(*(*  let library = create_library "Main Library" in*)*)
-(*(*  match library with*)*)
-(*(*  | Buyable { type_square = Library l; _ } ->*)*)
-(*(*      check string "Nom de la bibliothèque" "Main Library" (get_name_restaurant l)*)*)
-(*(*  | _ -> fail "Création de bibliothèque échouée"*)*)
+(* Création du tableau `board_example` avec des objets `square` *)
+let board_example = [|
+  Buyable example_library;
+  Buyable example_restaurant;
+  Buyable example_course;
+  create_tax 50 "Taxe";
+  House;
+  Cheating
+|]
 
-(*(* Test de création et accès aux attributs d'un restaurant *)*)
-(*let test_create_restaurant () =*)
-(*  let restaurant = create_restaurant "Gourmet Bistro" in*)
-(*  match restaurant with*)
-(*  | Buyable { type_square = Restaurant r; _ } ->*)
-(*      check string "Nom du restaurant" "Gourmet Bistro" (get_name_restaurant r)*)
-(*  | _ -> fail "Création de restaurant échouée"*)
+(* Tests pour la fonction `price_buyable` *)
+let test_price_buyable () =
+  let example_course_cours = Option.get (get_cours example_course) in
+  let example_library_type = get_type_square example_library in
+  let example_restaurant_type = get_type_square example_restaurant in
 
-(*(* Test du calcul du prix d'achat d'une propriété *)*)
-(*let test_price_buyable () =*)
-(*  let cours = create_cours Math 120 "Maths 101" in*)
-(*  let library = create_library "Central Library" in*)
-(*  let restaurant = create_restaurant "Cafe 101" in*)
-(*  match (cours, library, restaurant) with*)
-(*  | Buyable { type_square = Cours c; _ }, Buyable { type_square = Library _; _ }, Buyable { type_square = Restaurant _; _ } ->*)
-(*      check int "Prix d'un cours" 120 (price_buyable (Cours c));*)
-(*      check int "Prix d'une bibliothèque" 200 (price_buyable (Library {name = "Test Library"}));*)
-(*      check int "Prix d'un restaurant" 150 (price_buyable (Restaurant {name = "Test Restaurant"}))*)
-(*  | _ -> fail "Calcul du prix échoué"*)
+  (* Vérification du prix de la bibliothèque *)
+  (match example_library_type with
+  | Library lib -> check int "Prix de la bibliothèque" 200 (price_buyable (Library lib))
+  | _ -> fail "Le type extrait n'est pas une bibliothèque");
 
-(*(* Test du calcul de la taxe *)*)
-(*let test_get_tax_amount () =*)
-(*  let tax_square = create_tax 300 "Taxe sur la richesse" in*)
-(*  check int "Montant de la taxe" 300 (get_tax_amount tax_square);*)
-(*  let house = House in*)
-(*  check int "Taxe sur une case non taxable" 0 (get_tax_amount house)*)
+  (* Vérification du prix du restaurant *)
+  (match example_restaurant_type with
+  | Restaurant r -> check int "Prix du restaurant" 150 (price_buyable (Restaurant r))
+  | _ -> fail "Le type extrait n'est pas un restaurant");
 
-(*(* Test de changement de propriétaire d'une propriété achetable *)*)
-(*let test_change_owner () =*)
-(*  let cours = create_cours Math 120 "Maths 101" in*)
-(*  match cours with*)
-(*  | Buyable b ->*)
-(*      let cours_with_owner = change_owner b (Some 1) in*)
-(*      check (option int) "Propriétaire modifié" (Some 1) (match cours_with_owner with Buyable b -> b.proprietaire_index | _ -> None)*)
-(*  | _ -> fail "Modification du propriétaire échouée"*)
+  (* Vérification du prix du cours *)
+  check int "Prix du cours" 100 (price_buyable (Cours example_course_cours))
 
-(*(* Test du nom d'une case *)*)
-(*let test_name_square () =*)
-(*  let cours = create_cours Math 120 "Maths 101" in*)
-(*  let tax_square = create_tax 300 "Wealth Tax" in*)
-(*  check string "Nom d'une case de cours" "Maths 101" (name_square cours);*)
-(*  check string "Nom d'une case de taxe" "Wealth Tax" (name_square tax_square);*)
-(*  check string "Nom d'une case maison" "Maison" (name_square House);*)
-(*  check string "Nom d'une case email" "Email" (name_square Email)*)
+(* Tests pour `create_library`, `create_restaurant`, et `create_cours` *)
+let test_create_course () =
+  let course_square = create_cours Math 100 "Mathématiques" 20 40 80 160 50 in
+  match course_square with
+  | Buyable b ->
+    let course = Option.get (get_cours b) in
+    check string "Nom du cours" "Mathématiques" (get_name_cours course);
+    check int "Prix du cours" 100 (price_buyable (Cours course))
+  | _ -> fail "La création du cours a échoué"
 
-(*(* Groupe de tests pour le module Square *)*)
-(*let () =*)
-(*  run "Square tests" [*)
-(*(*    "Cours", [test_case "Création d'un cours" `Quick test_create_cours];*)*)
-(*(*    "Library", [test_case "Création d'une bibliothèque" `Quick test_create_library];*)*)
-(*    "Restaurant", [test_case "Création d'un restaurant" `Quick test_create_restaurant];*)
-(*    "Price", [test_case "Calcul du prix d'achat" `Quick test_price_buyable];*)
-(*    "Tax", [test_case "Montant de la taxe" `Quick test_get_tax_amount];*)
-(*    "Owner", [test_case "Changement de propriétaire" `Quick test_change_owner];*)
-(*    "Square Name", [test_case "Nom de la case" `Quick test_name_square];*)
-(*  ]*)
+(* Test pour `change_owner` *)
+let test_change_owner () =
+  let new_owner_course = change_owner example_course (Some 1) in
+  match new_owner_course with
+  | Buyable b -> check (option int) "Nouveau propriétaire" (Some 1) b.proprietaire_index
+  | _ -> fail "Échec du changement de propriétaire"
+
+(* Tests pour `get_tax_amount` *)
+let test_get_tax_amount () =
+  check int "Montant de la taxe" 50 (get_tax_amount (board_example.(3)));
+  check int "Montant d'une case non Tax" 0 (get_tax_amount (board_example.(4)))
+
+(* Tests pour `name_square` *)
+let test_name_square () =
+  check string "Nom de la bibliothèque" "Library" (name_square (board_example.(0)));
+  check string "Nom du restaurant" "Restaurant" (name_square (board_example.(1)));
+  check string "Nom de la case Tax" "Taxe" (name_square (board_example.(3)))
+
+(* Test pour `update_degre` *)
+let test_update_degre () =
+  let updated_course = update_degre example_course in
+  match get_type_square updated_course with
+  | Cours c -> check string "Diplôme mis à jour en Licence" "Licence" (match (get_degre c) with
+    | Some Licence -> "Licence"
+    | Some Master -> "Master"
+    | Some Doctorat -> "Doctorat"
+    | None -> "Pas de diplôme")
+  | _ -> fail "Le type extrait n'est pas un cours"
+
+(* Tests pour `get_index_from_square` et `get_index_from_square_buyable` *)
+let test_get_index_from_square () =
+  check (option int) "Index de la bibliothèque" (Some 0) (get_index_from_square (board_example.(0)) board_example);
+  check (option int) "Index de la taxe" (Some 3) (get_index_from_square (board_example.(3)) board_example)
+
+let test_get_index_from_square_buyable () =
+  check (option int) "Index de la bibliothèque en tant que Buyable" (Some 0) (get_index_from_square_buyable example_library board_example);
+  check (option int) "Index du restaurant en tant que Buyable" (Some 1) (get_index_from_square_buyable example_restaurant board_example)
+
+(* Lancer les tests *)
+let () =
+  run "Tests pour Square" [
+    "Création et prix", [
+      test_case "Prix des Buyables" `Quick test_price_buyable;
+      test_case "Créer un cours" `Quick test_create_course;
+      test_case "Changer propriétaire" `Quick test_change_owner;
+    ];
+    "Propriétés des cases", [
+      test_case "Montant de la taxe" `Quick test_get_tax_amount;
+      test_case "Nom des cases" `Quick test_name_square;
+    ];
+    "Diplômes et prix d'atterrissage", [
+      test_case "Mise à jour du diplôme" `Quick test_update_degre;
+    ];
+    "Index dans le plateau", [
+      test_case "Index d'une case" `Quick test_get_index_from_square;
+      test_case "Index d'un Buyable" `Quick test_get_index_from_square_buyable;
+    ];
+  ]
